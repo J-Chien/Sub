21a097b6db50f65657db8f58f250aec5a2120665
J-Chien <53326335+J-Chien@users.noreply.github.com>
2024-06-24 23:58:29 +0800

    Update tap_getCookie.js

--- tap_getCookie.js
+++ tap_getCookie.js
@@ -1,5 +1,5 @@
 if ($request && $request.method != 'OPTIONS') {
-    const cookie = $request.headers['cookie'];
+    const cookie = $request.headers['Cookie'];
     console.log(cookie);
     $persistentStore.write(cookie, 'SHENSI_Cookie');
     $notification.post('getCookie success', 'Stored in SHENSI_Cookie', cookie);
