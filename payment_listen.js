5f75e5183b22b86661f61ad4ea7f6246c47d3713
J-Chien <53326335+J-Chien@users.noreply.github.com>
2024-06-25 00:55:16 +0800

    Update payment_listen.js

--- payment_listen.js
+++ payment_listen.js
@@ -5,13 +5,6 @@ class jamie {
             'Content-Type': 'application/json',
             'cookie': $persistentStore.read('SHENSI_Cookie'),
         };
-        this.mapping = {
-            "sold_products": "已售商品",
-            "collaborated_creators_num": "建联达人",
-            "estimated_partner_commission": "预计佣金",
-            "promoted_creator_num": "发布达人",
-            "pending_payment": "待支付佣金",
-        };
     }
     httpAPI(config, params = {}, body = null) {
         const fullPath = config.path + '?' + this.parseParam({
@@ -63,87 +56,6 @@ class jamie {
 };
 
 const apiConfigs = {
-    // 获取进行中的 campaign
-    getOngoingCampaign: {
-        path: '/api/v1/affiliate/partner/campaign/list',
-        method: 'GET',
-        params: {
-            "status": 3,
-            "cur_page": 1,
-            "page_size": 50,
-            "crs_campaign_type": 3,
-        },
-    },
-    // 获取 campaign 信息
-    getCampaignDetail: {
-        path: '',
-        method: 'GET',
-        params: {
-            "campaign_id": "",
-        },
-    },
-    // 获取 campaign 下的产品列表
-    getCampaignProductList: {
-        path: '/api/v1/affiliate/partner/campaign/product_list/list',
-        method: 'GET',
-        params: {
-            "category_id": "",
-            "cur_page": 1,
-            "page_size": 20,
-        },
-    },
-    // 获取 campaign 的数据信息
-    getCampaignStatistics: {
-        path: '/api/v1/affiliate/partner/campaign/statistics',
-        method: 'GET',
-        params: {
-            "campaign_id": "",
-        },
-    },
-    // 获取商品信息
-    getProductStatistics: {
-        path: '/api/v1/affiliate/partner/campaign/product/statistics/list',
-        method: 'GET',
-        params: {
-            "campaign_id": "7372498587680458528",
-            "cur_page": 1,
-            "page_size": 100,
-        },
-    },
-    // 获取分类列表，传 0 为所有一级分类，共三级，传语言返回中文
-    getCategoryList: {
-        path: '/api/v1/affiliate/lux/product/category/children',
-        method: 'GET',
-        params: {
-            "category_id": 0,
-            "user_language": "zh-CN",
-        },
-    },
-    // 获取分类下的产品列表（筛选）
-    getCategoryProductList: {
-        path: '/api/v1/affiliate/partner/campaign/product/list',
-        method: 'GET',
-        params: {
-            "marked": false,
-            "category_ids": "",
-            "cur_page": 1,
-            "page_size": 50,
-            "campaign_id": "",
-        },
-    },
-    // 创建产品列表
-    createProductList: {
-        path: '/api/v1/affiliate/partner/campaign/product_list/create',
-        method: 'POST',
-        body: {
-            "name": '',
-            "campaign_id": "7372498587680458528",
-            "items": [{
-                product_id: '',
-                creator_commission_rate: '1300'
-            }],
-        }
-    },
     getPaymentInfo: {
         path: '/api/v1/oec/affiliate/agent/summary',
         method: 'GET',
@@ -163,7 +75,7 @@ const $ = new jamie();
         }
 
         if ($persistentStore.read('SHENSI_Payment_stat') === null) {
-            $persistentStore.write(currentPaymentInfo, 'SHENSI_Payment_stat');
+            $persistentStore.write(JSON.stringify(currentPaymentInfo), 'SHENSI_Payment_stat');
             console.log(缓存写入成功);
             $.done();
         }
@@ -185,12 +97,12 @@ const $ = new jamie();
             });
         }
 
-        $persistentStore.write(currentPaymentInfo, 'SHENSI_Payment_stat');
-
         if (notifications.length === 0) {
             console.log(`${$.formatTime(Date.now(), 'Asia/Taipei')} 监控，无更新`)
             $.done()
         }
+
+		$persistentStore.write(JSON.stringify(currentPaymentInfo), 'SHENSI_Payment_stat');
 
         notifications.forEach(notification => {
             $notification.post(notification.title, notification.subtitle, notification.body);
