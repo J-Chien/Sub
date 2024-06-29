d90afb4146734f2e0d16c3561c419fdf0865af01
J-Chien <53326335+J-Chien@users.noreply.github.com>
2024-06-30 05:44:47 +0800

    Update surge_rename.js

--- surge_rename.js
+++ surge_rename.js
@@ -1,28 +1,28 @@
-// 国家名映射，添加时移除空格
+// 国家名映射
 const countryMap = {
-    'Taiwan|台湾': 'TW',
-    'HongKong|香港': 'HK',
-    'Japan|日本': 'JP',
-    'UnitedStates|美国': 'US',
-    'Singapore|新加坡': 'SG',
-    'Canada': 'CA',
-    'UnitedKingdom': 'UK',
-    'Germany': 'DE',
-    'Netherlands': 'NL',
-    'Italy': 'IT',
-    'Spain': 'ES',
-    'Turkey': 'TR',
-    'Australia': 'AU',
-    'Argentina': 'AR',
-    'Brazil': 'BR',
-    'Chile': 'CL',
-    'Korea': 'KR',
-    'India': 'IN',
-    'Israel': 'IL',
-    'Thailand': 'TH',
-    'Vietnam': 'VN',
-    'Malaysia': 'MY',
-    'Johannesburg': 'ZA'
+    TW: ['Taiwan', '台湾'],
+    HK: ['Hong Kong', '香港'],
+    JP: ['Japan', '日本'],
+    US: ['United States', '美国'],
+    SG: ['Singapore', '新加坡'],
+    CA: ['Canada'],
+    UK: ['United Kingdom'],
+    DE: ['Germany'],
+    NL: ['Netherlands'],
+    IT: ['Italy'],
+    ES: ['Spain'],
+    TR: ['Turkey'],
+    AU: ['Australia'],
+    AR: ['Argentina'],
+    BR: ['Brazil'],
+    CL: ['Chile'],
+    KR: ['Korea'],
+    IN: ['India'],
+    IL: ['Israel'],
+    TH: ['Thailand'],
+    VN: ['Vietnam'],
+    MY: ['Malaysia'],
+    ZA: ['Johannesburg']
 };
 // $argument 映射
 const providersMap = {
@@ -39,38 +39,26 @@ try {
 } catch (error) {
     console.log(error);
 } finally {
-    $done({
-        body
-    });
-}
-
-function getCountryCode(match) {
-    // 遍历 countryMap 对象
-    for (const [key, value] of Object.entries(countryMap)) {
-        // 检查 key 是否包含 match
-        if (key.includes(match)) {
-            return value;
-        }
-    }
-    // 如果没有找到匹配项，返回 null
-    return null;
+    $done({ body });
 }
 
 function processData(body, provider) {
-    const lines = body.split('\n');
+    const lines = body.split('\n').filter(Boolean);
     const processedLines = lines.map(line => {
-        // 匹配节点名
-        const proxy = line.match(/^(.*?)=/);
+        const proxy = line.match(/^(.*?)\s(.*?)\d/);
         if (!proxy) {
             return line;
         }
-        const prefix = proxy[1].trim();
-        // 将空格跟 ‘|’ 移除
-        const sanitizedPrefix = prefix.replace(/[\s|]/g, '');
-        // 利用国家映射表进行替换
-        let countryCode = sanitizedPrefix.replace(new RegExp(Object.keys(countryMap).join('|'), 'gi'), country => ` ${providersMap[provider]} | ${getCountryCode(country)} `);
-        // 将替换后的国家代码重新插入到原始字符串中
-        return line.replace(prefix, countryCode);
+        let node = proxy[2];
+        for (const key in countryMap) {
+            const countryName = countryMap[key];
+            for (const i in countryName) {
+                if (node.indexOf(countryName[i]) != -1) {
+                    node = `${providersMap[provider]} | ${key} `;
+                }
+            }
+        }
+        return line.replace(proxy[2], node);
     });
     return processedLines.join('\n');
 }
